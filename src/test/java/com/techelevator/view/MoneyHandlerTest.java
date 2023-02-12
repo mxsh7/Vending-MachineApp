9dfb14aa09943a9e62d12856a204562936c7965d
Blake Ford <blakeford96@gmail.com>
2023-02-12 18:15:59 -0500

    AM/PM

---
+++ src/test/java/com/techelevator/view/MoneyHandlerTest.java
@@ -0,0 +1,23 @@
+package com.techelevator.view;
+
+import org.codehaus.plexus.util.cli.StreamFeeder;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoneyHandlerTest {
+
+    @Test
+    public void feedMoney() {
+//        feedMoney feedMoney = new feedMoney();
+
+    }
+
+    @Test
+    void selectProduct() {
+    }
+
+    @Test
+    void getChange() {
+    }
+}
